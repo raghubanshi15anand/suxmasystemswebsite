591efb21105861f290a82196153230f26ab8443c
anand <raghubanshi15anand@gmail.com>
2022-08-20 11:15:14 -0700

    Updates

---
+++ precache-manifest.83437fa02f124fc5c658b70b1a0fff9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d626299632cea3070786d843bda524",
+    "url": "/suxmasystemswebsite/index.html"
+  },
+  {
+    "revision": "87f40ea25a22d4b82aaf",
+    "url": "/suxmasystemswebsite/static/css/2.f7375132.chunk.css"
+  },
+  {
+    "revision": "1e423d4a5374048d93a3",
+    "url": "/suxmasystemswebsite/static/css/main.ed1cb385.chunk.css"
+  },
+  {
+    "revision": "87f40ea25a22d4b82aaf",
+    "url": "/suxmasystemswebsite/static/js/2.5c9724e6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/suxmasystemswebsite/static/js/2.5c9724e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e423d4a5374048d93a3",
+    "url": "/suxmasystemswebsite/static/js/main.dd890dbf.chunk.js"
+  },
+  {
+    "revision": "c23749c8807f149ee636",
+    "url": "/suxmasystemswebsite/static/js/runtime-main.8fd7a5ef.js"
+  }
+]);
